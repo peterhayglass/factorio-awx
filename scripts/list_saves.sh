7979c2e860f2d128ab4bf3141d14df93afe5cc0d
Peter Hayglass <peter.hayglass@gmail.com>
2021-09-09 01:34:57 -0500

    full path in list_saves.sh output

--- scripts/list_saves.sh
+++ scripts/list_saves.sh
@@ -2,10 +2,10 @@
 echo RECENT:
 cd /opt/factorio-saves/saves
 for FILE in *; do 
-    echo `date -d @"${FILE%.*}"` : ${FILE%.*}
+    echo `date -d @"${FILE%.*}"` : /opt/factorio-saves/saves/${FILE%.*}.zip
 done
 echo OLDER:
 cd /opt/factorio-saves/archive
 for FILE in *; do
-    echo `date -d @"${FILE%.*}"` : ${FILE%.*}
+    echo `date -d @"${FILE%.*}"` : /opt/factorio-saves/archive/${FILE%.*}.zip
 done
